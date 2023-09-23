584f0fb6e3d9c25a860e9a83afc10d58f3c03f52
Shane Li <a1595042264@gmail.com>
2023-09-23 15:31:13 -0400

    initialization

---
+++ CudaRuntime1/kernel.cu
@@ -0,0 +1,26 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+
+__global__ void printThreadIndices() {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    printf("blockIdx.x: %d, threadIdx.x: %d, tid: %d\n", blockIdx.x, threadIdx.x, tid);
+}
+
+
+int main() {
+    int threadsPerBlock = 4;
+    int blocksPerGrid = 3;
+
+    // Launch the kernel
+    printThreadIndices<<<blocksPerGrid, threadsPerBlock>>>();
+
+    // Ensure all CUDA operations are completed before proceeding
+    cudaDeviceSynchronize();
+
+    return 0;
+}
